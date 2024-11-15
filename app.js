aca4f665d505559c5dd9176adbca3d3b9168c72d
ladiez024 <ladiez03@ucm.es>
2024-11-15 22:03:14 +0100

    para github

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const rutas = require('./router');//importa el archivo router.js
 app.set('view engine', 'ejs');//configura EJS como el motor de plantillas de vistas
 app.set('views', path.join(__dirname, 'views'));//configura la ubicación de la carpeta llamada views 
 
+const ejs = require('ejs');
+const fs = require('fs');
+ejs.renderFile(path.join(__dirname, 'views', 'index.ejs'), {}, (err, str) => {
+    if (err) {
+        console.error(err);
+    } else {
+        fs.writeFileSync(path.join(__dirname, 'public', 'index.html'), str);
+        console.log('index.html generado con éxito');
+    }
+});
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));//habilita el procesamiento de datos de formularios
 app.use(express.json());//habilita el procesamiento de datos en formato JSON
